475557bc8438dd760c8b5b9c7bdb0b4f239be505
hakimoss <hakimos@live.ca>
2022-06-15 13:04:36 -0400

    formulaire d'evoi de mail dans le footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './footer.css';
+import logo from '../images/logox512.png';
 
 
 const Footer = () => {
 
     return(
         <div className='ctnFooter'>
-            Footer
+            <div className='ctnFormSendEmail'>
+                <img className='weedQcLogoFooter' src={logo} alt="weed qc logo" />
+                <h3 style={{ color:'white'}}>Pour tout commentaire ou signalment de bug veuillez remplire se formulaire afin de menvoyer un mail !</h3>
+                <form className='formSendEmail' action="mailto:hakimos@live.ca" method="post" enctype="text/plain">
+                    
+                        <div><label htmlFor='FirstName' style={{ color: 'white'}}>FirstName:</label></div>
+                        <input type="text" name="FirstName" id='FirstName' className='firstNameInputSendEmail'/> 
+                
+                        <div><label htmlFor='Email' style={{ color: 'white'}}>email:</label></div>
+                        <input type="text" name="email" className='emailInputSendEmail' id='Email'/>
+
+                        <div><label style={{ color: 'white'}}>message:</label></div>
+                        <textarea name="message" className='messageInputSendEmail'/> 
+
+                        <div><input type="submit" name="submit" className='btnSubmitSendEmail' value="Submit" /></div>
+                </form>
+            </div>
         </div>
     )
 }
